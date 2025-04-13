27b1862eff4496d397f5d91f7c69a91a527f099c
Nishant lamba <131985675+nishantlamba11@users.noreply.github.com>
2025-04-13 13:04:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-//your JS code here. If required.
+let output = document.querySelector("#output");
+
+let promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve([1, 2, 3, 4]);
+  }, 3000);
+});
+
+promise
+  .then(arr => {
+    let evenNum = arr.filter(num => num % 2 === 0);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(evenNum);
+      }, 1000);
+    });
+  })
+  .then(filteredArr => {
+    output.textContent = filteredArr.join(",");
+    let doubled = filteredArr.map(num => num * 2);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(doubled);
+      }, 2000);
+    });
+  })
+  .then(finalArr => {
+    output.textContent = finalArr.join(",");
+  })
+  .catch(err => {
+    console.log(err);
+    output.textContent = "Error: " + err;
+  });
